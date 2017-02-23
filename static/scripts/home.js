41c7a5ed19d45c000c0fd30a753c605ad3cb1fdb
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2017-02-22 23:14:05 -0800

    return in form validation

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,15 +1,22 @@
-function validate_form(){
+function validateForm( event ){
 	console.log("ok we are validating form");
 	console.log("check the lenth and check the type of the input fields");
+	return true;
 }
 
 function validateType(){
 	console.log("checks the type");
 	console.log("check integer and check string");
+	
 }
 
-function checkLength(){
-	console.log("chekc the lenght is > 0");	
+function validateLength(){
+	console.log("chekc the length  > 0");	
 }
 
+var form = document.getElementById("new-pet-form");
+
+console.log( form );
+
+// form.addEventListener( 'submit', validateForm);
 
