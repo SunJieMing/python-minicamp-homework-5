466a2116516c73f5e1487998402d7e325884ac3a
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2017-02-22 23:01:34 -0800

    some messages

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,3 +1,15 @@
 function validate_form(){
-	console.log("ok we are validating form")
+	console.log("ok we are validating form");
+	console.log("check the lenth and check the type of the input fields");
 }
+
+function validateType(){
+	console.log("checks the type");
+	console.log("check integer and check string");
+}
+
+function checkLength(){
+	console.log("chekc the lenght is > 0");	
+}
+
+
