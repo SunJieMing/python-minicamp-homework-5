e7a775b2d2ce962f2fd3bba7d6b131f0aedef0cb
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2017-02-22 23:32:51 -0800

    add the form inputs

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,6 +1,15 @@
 function validateForm( event ){
+	
+	var name = document.getElementById("nameInput");
+	var species = document.getElementById("speciesInput");
+	var age = document.getElementById("ageInput");
+	
+	console.log("name species input", name, species, input);
+	
 	console.log("ok we are validating form");
 	console.log("check the lenth and check the type of the input fields");
+	validateType();
+	validateLength();
 	return true;
 }
 
@@ -14,9 +23,9 @@ function validateLength(){
 	console.log("chekc the length  > 0");	
 }
 
-var form = document.getElementById("new-pet-form");
+// var form = document.getElementById("new-pet-form");
 
-console.log( form );
+// console.log( form );
 
 // form.addEventListener( 'submit', validateForm);
 
